0b287668281b9096e5d598d650de9565044b4980
simple <shwncho4@gmail.com>
2022-08-17 00:23:01 +0900

    Feat: Find Bean by name and type

---
+++ src/test/java/com/example/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,50 @@
+package com.example.core.beanfind;
+
+import com.example.core.AppConfig;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    //구체 타입으로 조회할 경우는 변경시 유연성이 떨어지기 때문에 안좋은 코드다.
+    // 그렇지만 생각치 못한 에러가 나왔을 때 확인용으로 사용하기위해 알아두자
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService",MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX(){
+        //ac.getBean("xxxxx", MemberService.class);
+        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()-> ac.getBean("xxxxx", MemberService.class));
+    }
+}
