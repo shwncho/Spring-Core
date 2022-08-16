7b453e09ed2be3ec06cd915f326da88a8a6ca064
simple <shwncho4@gmail.com>
2022-08-16 20:21:31 +0900

    Feat: findAllBean, findApplicationBean

---
+++ src/test/java/com/example/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,39 @@
+package com.example.core.beanfind;
+
+import com.example.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object= " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION : 일반적으로 사용자가 정의한 빈
+            //ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            //외부 라이브러리나 내가 등록한 빈을 출력
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object= " + bean);
+            }
+        }
+    }
+}
